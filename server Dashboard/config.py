884332762867838a05281fbc5843e82d666b26c5
Tejaram Chaudhari <121306030+tejaschaudhari131@users.noreply.github.com>
2024-08-28 17:31:22 +0530

    Revert "review"

--- server Dashboard/config.py
+++
@@ -1,4 +0,0 @@
-# config.py
-FLASK_SERVER_IP = '0.0.0.0'  # Change to your server's IP address
-FLASK_SERVER_PORT = 5000  # Change to an available port if needed
-ROBOT_IP = '192.168.1.101'  # Default IP of the robot
